a453e20a12ffd1f87b60408a5c5c7fca593c582a
SaraToth <saratoth101@gmail.com>
2025-01-21 08:36:19 +0900

    Build: Install project dependencies and Jest
    Install and configure webpack, jest and babel, configure package JSON and gitignore files to create a build environment for ES6 modules and testing.

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html"],
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+}
