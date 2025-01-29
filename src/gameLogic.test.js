aed7d5bb8bf653321a2873f1e1b76302a488d9ce
SaraToth <saratoth101@gmail.com>
2025-01-29 19:47:02 +0900

    Test: Check ship initializes with correct length
    Confirm that the length value input as a parameter is correctly initialized.

--- src/gameLogic.test.js
+++ src/gameLogic.test.js
@@ -1,19 +1,7 @@
-import { newShip } from "./gameLogic.js";
 import { ship } from "./gameLogic.js";
 
-describe("Checks newShip function", ()=> {
-    it("Returns the hits", ()=> {
-        expect(newShip(5)).toStrictEqual({length: 5, hits: 0, isSunk: false});
-    })
-})
-
-describe("ship function", ()=> {
-    it("Returns individual ship objects", ()=> {
-        expect(ship().battleship).toBeDefined();
-        expect(ship().battleship).toStrictEqual({length: 4, hits: 0, isSunk: false});
-        expect(ship().aircraftCarrier).toStrictEqual({length: 5, hits: 0, isSunk: false});
-        expect(ship().cruiser).toStrictEqual({length: 3, hits: 0, isSunk: false});
-        expect(ship().submarine).toStrictEqual({length: 3, hits: 0, isSunk: false});
-        expect(ship().destroyer).toStrictEqual({length: 2, hits: 0, isSunk: false});
+describe("Ship Factory Function", ()=> {
+    it("Ship initializes with correct length", () => {
+        expect(ship(3).length).toBe(3);
     })
 })
