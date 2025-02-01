b9a7fcb94e93db7015de7d55d21f89d4838a1363
SaraToth <saratoth101@gmail.com>
2025-02-01 16:17:36 +0900

    Feat: Write selectShip and begin write placeship
    selectShip will be needed to target the correct ship when placing a ship on the board, or choosing the correct ship to send hits to. It is the first step needed in the placeShip function.

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -21,9 +21,36 @@ export const ship = (length) => {
 export const gameBoard = () => {
     const board = Array.from({length: 10}, ()=> Array(10).fill(false));
 
+    const aircraftCarrier = ship(5);
+    const battleship = ship(4);
+    const cruiser = ship(3);
+    const submarine = ship(3);
+    const destroyer = ship(2);
+
+    const selectShip = (shipname) => {
+        if(shipname == "aircraftCarrier") {
+            return aircraftCarrier;
+        } else if(shipname == "battleshp") {
+            return battleship;
+        } else if(shipname == "cruiser") {
+            return cruiser;
+        } else if(shipname == "submarine") {
+            return submarine;
+        } else if(shipname == "destroyer") {
+            return destroyer;
+        }
+    }
+
     return {
         getBoard() {
             return board;
+        },
+        placeship(shipname) {
+            const ship = selectShip(shipname);
+            
+
+
+            return ship;
         },
         recieveAttack(coordinate) {
             const [x, y] = coordinate;
@@ -33,6 +60,6 @@ export const gameBoard = () => {
             } else if(board[x][y] == "miss") {
                 throw new Error("You already tried that spot, choose another.");
             }
-        }
+        },
     }
 }
