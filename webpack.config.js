5ae62bd5a62bfef5cd13191bf42dbca7810b56bd
SaraToth <saratoth101@gmail.com>
2025-02-02 18:39:34 +0900

    Build: Create stylesheet and config with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -18,4 +18,13 @@ module.exports = {
             template: "./src/template.html",
         }),
     ],
+
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+        ],
+      },
 }
