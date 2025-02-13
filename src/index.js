443b241a8804787062b79a9cfa899218e4cbfd34
SaraToth <saratoth101@gmail.com>
2025-02-13 19:02:06 +0900

    Feat: Start button toggles to choose player screen
    Write button event listeners that toggle from the start game screen to the choose players screen and write temporary alerts to confirm 1 players and 2 players buttons work.

--- src/index.js
+++ src/index.js
@@ -6,14 +6,37 @@ import { screenController } from "./screenController.js";
 const display = screenController();
 display.displayBoard();
 
+const header = document.querySelector("header");
+const headerText = document.querySelector("h1");
 const btnStart = document.querySelector(".btn-start");
-const modalStart = document.querySelector(".game-start-modal");
+const start = document.querySelector(".game-start-modal");
 
-const modalChoosePlayer = document.querySelector(".choose-player");
+const ChoosePlayer = document.querySelector(".choose-player");
+const btnOnePlayers = document.querySelector(".btn-one-players");
+const btnTwoPlayers = document.querySelector(".btn-two-players");
 
 
 btnStart.addEventListener("click", ()=> {
-    modalStart.style.visibility = "hidden";
-    modalChoosePlayer.style.visibility = "visible";
+    start.style.visibility = "hidden";
+    ChoosePlayer.style.visibility = "visible";
+    header.style.visibility = "visible";
+    headerText.textContent = "Choose how many players";
 
 })
+
+btnOnePlayers.addEventListener("click", ()=> {
+    alert("one player game vs computer");
+    //Indicate the game is against computer
+    //hide choose players
+    //show player one place ships
+})
+
+btnTwoPlayers.addEventListener("click", ()=> {
+    alert("two player game");
+    //indicate the game is two people playing against one another side by side
+    //hide choose players
+    //show player one place ships
+    
+    //Then show player two place ships
+})
+
