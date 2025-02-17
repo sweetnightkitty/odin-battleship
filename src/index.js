162351280850f22fc0da91319bc06ce8f7221764
SaraToth <saratoth101@gmail.com>
2025-02-17 18:57:53 +0900

    Refactor: screenToggler to screenController node
    Simplify index.js by refactoring screenToggler factory function to the screenController node where all UI is handled.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import "./styles.css";
-import { screenController } from "./screenController.js";
+import { controller } from "./screenController.js";
+import { screenToggler } from "./screenController.js";
 
-//INITIATE GAME:
-const controller = screenController();
-
-//-------------------------------------------------------------------------
-
-//HTML CONSTANTS:
-
-const gamePlayerOne = document.querySelector(".game-player-one");
-const endPlayerOneRound = document.querySelector(".end-player-one");
-
-const gamePlayerTwo = document.querySelector(".game-player-two");
-const endPlayerTwoRound = document.querySelector(".end-player-two");
-
-const startRound = document.querySelector(".start-round");
-const startRoundBtn = document.querySelector(".start-round-btn");
+//INITIATES SCREEN TOGGLER:
+const toggler = screenToggler();
 
 //-------------------------------------------------------------------------
 
@@ -27,59 +15,18 @@ controller.displayShips(); //Displays the active player's bottom board (their sh
 
 //-------------------------------------------------------------------------
 
+//BUTTONS:
+
+const endPlayerOneRound = document.querySelector(".end-player-one");
+const endPlayerTwoRound = document.querySelector(".end-player-two");
+const startRoundBtn = document.querySelector(".start-round-btn");
+
+//-------------------------------------------------------------------------
+
 //BUTTON EVENTS:
 
 //Toggles between playerOne's UI game, intermediate startRound, and playerTwo's UI game
-endPlayerOneRound.addEventListener("click", goToStartRound);
-endPlayerTwoRound.addEventListener("click", goToStartRound);
-startRoundBtn.addEventListener("click", nextPlayerRound);
+endPlayerOneRound.addEventListener("click", toggler.goToStartRound);
+endPlayerTwoRound.addEventListener("click", toggler.goToStartRound);
+startRoundBtn.addEventListener("click", toggler.nextPlayerRound);
 
-
-function goToStartRound() {
-   if(event.target.classList[1] == "end-player-one") {
-        //Hide player One
-        gamePlayerOne.style.display = "none";
-
-        //Show intermediate screen
-        startRound.style.display = "flex";
-
-        //Add a class to the intermediate screen to know who plays next
-        if(startRoundBtn.classList.contains("one")) {startRoundBtn.classList.remove("one")};
-        startRoundBtn.classList.add("two");
-        startRoundBtn.textContent = "Player Two Start";
-
-   } else if(event.target.classList[1] == "end-player-two") {
-        //Hide Player Two
-        gamePlayerTwo.style.display = "none";
-
-        //Shows Intermediate Screen
-        startRound.style.display = "flex";
-
-        //Add a class to the intermediate screen to know who plays next
-        startRoundBtn.classList.add("one");
-        startRoundBtn.classList.remove("two");
-        startRoundBtn.textContent = "Player One Start";
-
-   }
-}
-
-function nextPlayerRound() {
-
-    //Hide the intermediate screen
-    startRound.style.display = "none";
-
-    //Regenerate the game for current active player
-    controller.switchPlayers();
-    controller.reset("board");
-    controller.reset("ships");
-    controller.displayBoard();
-    controller.displayShips(); // may be an issue
-    
-    //Displays the correct person's board:
-    const nextPlayer = event.target.classList[1];
-    if(nextPlayer == "one") {
-        gamePlayerOne.style.display = "flex";
-    } else if(nextPlayer == "two") {
-        gamePlayerTwo.style.display = "flex";
-    }
-}
--- src/screenController.js
+++ src/screenController.js
@@ -1,6 +1,6 @@
 import { player } from "./gameLogic";
 
-export const screenController = () => {
+const screenController = () => {
     const playerOne = player();
     const playerTwo = player();
     const playerOneDisplay = document.querySelector(".player-one-board");
@@ -135,3 +135,64 @@ export const screenController = () => {
         
     }
 }
+
+export const controller = screenController();
+
+export const screenToggler = () => {
+    const gamePlayerOne = document.querySelector(".game-player-one");
+
+    const gamePlayerTwo = document.querySelector(".game-player-two");
+
+    const startRound = document.querySelector(".start-round");
+    const startRoundBtn = document.querySelector(".start-round-btn");
+    return {
+        goToStartRound() {
+            if(event.target.classList[1] == "end-player-one") {
+                 //Hide player One
+                 gamePlayerOne.style.display = "none";
+         
+                 //Show intermediate screen
+                 startRound.style.display = "flex";
+         
+                 //Add a class to the intermediate screen to know who plays next
+                 if(startRoundBtn.classList.contains("one")) {startRoundBtn.classList.remove("one")};
+                 startRoundBtn.classList.add("two");
+                 startRoundBtn.textContent = "Player Two Start";
+         
+            } else if(event.target.classList[1] == "end-player-two") {
+                 //Hide Player Two
+                 gamePlayerTwo.style.display = "none";
+         
+                 //Shows Intermediate Screen
+                 startRound.style.display = "flex";
+         
+                 //Add a class to the intermediate screen to know who plays next
+                 startRoundBtn.classList.add("one");
+                 startRoundBtn.classList.remove("two");
+                 startRoundBtn.textContent = "Player One Start";
+         
+            }
+         },
+
+        nextPlayerRound () {
+                //Hide the intermediate screen
+                startRound.style.display = "none";
+            
+                //Regenerate the game for current active player
+                controller.switchPlayers();
+                controller.reset("board");
+                controller.reset("ships");
+                controller.displayBoard();
+                controller.displayShips(); // may be an issue
+                
+                //Displays the correct person's board:
+                const nextPlayer = event.target.classList[1];
+                if(nextPlayer == "one") {
+                    gamePlayerOne.style.display = "flex";
+                } else if(nextPlayer == "two") {
+                    gamePlayerTwo.style.display = "flex";
+                }
+        },
+    }
+} 
+
