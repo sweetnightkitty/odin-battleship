116e9c10aa47e412f0e990777e2b0978be3107dc
SaraToth <saratoth101@gmail.com>
2025-02-17 18:09:15 +0900

    Feat: Write two functions that handle pass play
    goToStartRound toggles from either player one or player two's gameboard to the startRound intermediate div.
    nextPlayerRound toggles from the startRound intermediate div to the correct player's gameboard.
    These functions are added as btn event listeners and toggle game play between two players.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,16 @@ const controller = screenController();
 
 //HTML CONSTANTS:
 
+const gamePlayerOne = document.querySelector(".game-player-one");
 const endPlayerOneRound = document.querySelector(".end-player-one");
+const hideShipsOne = document.querySelector(".player-one-ships");
+
+const gamePlayerTwo = document.querySelector(".game-player-two");
+const endPlayerTwoRound = document.querySelector(".end-player-two");
+const hideShipsTwo = document.querySelector(".player-two-ships");
+
+const startRound = document.querySelector(".start-round");
+const startRoundBtn = document.querySelector(".start-round-btn");
 
 //-------------------------------------------------------------------------
 
@@ -19,6 +28,56 @@ controller.displayShips(); //Displays the active player's bottom board (their sh
 //-------------------------------------------------------------------------
 
 //BUTTON EVENTS:
-endPlayerOneRound.addEventListener("click", ()=> {
+endPlayerOneRound.addEventListener("click", goToStartRound);
+endPlayerTwoRound.addEventListener("click", goToStartRound);
+startRoundBtn.addEventListener("click", nextPlayerRound);
 
-})
+
+function goToStartRound() {
+   if(event.target.classList[1] == "end-player-one") {
+        //Hide player One
+        gamePlayerOne.style.display = "none";
+
+        //Show intermediate screen
+        startRound.style.display = "flex";
+
+        //Add a class to the intermediate screen to know who plays next
+        if(startRoundBtn.classList.contains("one")) {startRoundBtn.classList.remove("one")};
+        startRoundBtn.classList.add("two");
+        startRoundBtn.textContent = "Player Two Start";
+
+   } else if(event.target.classList[1] == "end-player-two") {
+        //Hide Player Two
+        gamePlayerTwo.style.display = "none";
+
+        //Shows Intermediate Screen
+        startRound.style.display = "flex";
+
+        //Add a class to the intermediate screen to know who plays next
+        startRoundBtn.classList.add("one");
+        startRoundBtn.classList.remove("two");
+        startRoundBtn.textContent = "Player One Start";
+
+   }
+}
+
+function nextPlayerRound() {
+
+    //Hide the intermediate screen
+    startRound.style.display = "none";
+
+    //Regenerate the game for current active player
+    controller.switchPlayers();
+    controller.reset("board");
+    controller.reset("ships");
+    controller.displayBoard();
+    controller.displayShips(); // may be an issue
+    
+    //Displays the correct person's board:
+    const nextPlayer = event.target.classList[1];
+    if(nextPlayer == "one") {
+        gamePlayerOne.style.display = "flex";
+    } else if(nextPlayer == "two") {
+        gamePlayerTwo.style.display = "flex";
+    }
+}
