cd29e10499d29976215381ffebcdfdc9a2f131d4
SaraToth <saratoth101@gmail.com>
2025-02-17 15:56:35 +0900

    Feat: Display the players Ships on the gameboard
    Called the displayShip function, to display Ships for the player below their gameboard.

--- src/index.js
+++ src/index.js
@@ -8,13 +8,17 @@ const controller = screenController();
 
 //HTML CONSTANTS:
 
-
+const endPlayerOneRound = document.querySelector(".end-player-one");
 
 //-------------------------------------------------------------------------
 
 //GAME PLAY:
 controller.displayBoard(); //default displays player ones board
+controller.displayShips();
 
 //-------------------------------------------------------------------------
 
 //BUTTON EVENTS:
+endPlayerOneRound.addEventListener("click", ()=> {
+
+})
