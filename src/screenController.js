f1e3da4c404672d982dcea1ae3f0442751510f21
SaraToth <saratoth101@gmail.com>
2025-02-17 15:55:02 +0900

    Feat: Create a display to display ships only.
    During game play, both players will need to simaltaneously see their record of hits and misses against their opponent - created via displayBoard, but also need to view their own ships, and track their hits. So displayShip will display this board.

--- src/screenController.js
+++ src/screenController.js
@@ -4,6 +4,9 @@ export const screenController = () => {
     const playerOne = player();
     const playerTwo = player();
     const playerOneDisplay = document.querySelector(".player-one-board");
+    const playerTwoDisplay = document.querySelector(".player-two-board");
+    const playerOneShips = document.querySelector(".player-one-ships");
+    const playerTwoShips = document.querySelector(".player-two-ships");
 
     const players = [
         {
@@ -11,13 +14,15 @@ export const screenController = () => {
             opponent: playerTwo,
             name: "one",
             display: playerOneDisplay,
+            shipDisplay: playerOneShips,
         },
 
         {
             activePlayer: playerTwo,
             opponent: playerOne,
-            name: "one", //computer playing
-            display: playerOneDisplay // computer playing
+            name: "two", //computer playing
+            display: playerTwoDisplay, // computer playing
+            shipDisplay: playerTwoShips,
         }
     ]
 
@@ -56,7 +61,7 @@ export const screenController = () => {
 
     return {
         //Creates UI buttons for both boards, playerOne is the default staring player
-        displayBoard(displayBoard = playerOneDisplay) {
+        displayBoard() {
             const opponentBoard = activePlayer.opponent.getBoard();
             const name = activePlayer.name;
 
@@ -65,12 +70,13 @@ export const screenController = () => {
                     const button = document.createElement("button");
                     button.classList.add(`player-${name}-buttons`, `${i}${j}`);
                     applyColor(i, j, opponentBoard, button);
-                    displayBoard.appendChild(button);
+                    activePlayer.display.appendChild(button);
                 }
             }
 
             //Buttons must be defined AFTER displayBoard generates new buttons.
             const playerOneButtons = document.querySelectorAll(".player-one-buttons");
+            const playerTwoButtons = document.querySelectorAll(".player-two-buttons");
 
             //Need to ensure the event listener is added every time new buttons display.
             playerOneButtons.forEach(button => {
@@ -79,15 +85,22 @@ export const screenController = () => {
                     this.resetBoard(playerOneDisplay);
                     this.displayBoard(playerOneDisplay);
 
-                    //Checks if game is over
-                    if(activePlayer.opponent.isGameOver()) {
-                        alert("Game Over!");
-                    }
-
-                    switchPlayers();
-
                 })
             })
+        },
+
+        displayShips() {
+            const playerBoard = activePlayer.activePlayer.getBoard();
+            const name = activePlayer.name;
+
+            for(let i = 0; i < playerBoard.length; i++) {
+                for(let j = 0; j < playerBoard[i].length; j++) {
+                    const button = document.createElement("button");
+                    button.classList.add(`player-${name}-ship-buttons`, `${i}${j}`);
+                    //Need to create color for the ships / hits
+                    activePlayer.shipDisplay.appendChild(button);
+                }
+            }
         },
 
         resetBoard(displayBoard) {
@@ -101,6 +114,7 @@ export const screenController = () => {
             activePlayer.opponent.recieveAttack([x, y]);
             
             //switch turns
-        }
+        },
+        
     }
 }
