bf4404b9e989013549932de86e01229c8f44066f
SaraToth <saratoth101@gmail.com>
2025-03-02 19:04:10 +0900

    Perf: Remove userPlacesShip logic
    Since ship placement was converted to drag and drop, userplacesship feature was no longer needed, and thus deleted along with it's relevant use in eventListeners.

--- src/screenController.js
+++ src/screenController.js
@@ -342,15 +342,6 @@ const screenController = () => {
             displayDiv.appendChild(submarine);
             displayDiv.appendChild(destroyer);
 
-           //Add event listeners here
-
-           //TO BE PHASED OUT:
-           aircraftCarrier.addEventListener("click", this.userPlacesShip);
-           battleship.addEventListener("click", this.userPlacesShip);
-           cruiser.addEventListener("click", this.userPlacesShip);
-           submarine.addEventListener("click", this.userPlacesShip);
-           destroyer.addEventListener("click", this.userPlacesShip);
-
            //DRAG AND DROP LISTENERS
            aircraftCarrier.addEventListener("dragstart", dragStart);
            aircraftCarrier.addEventListener("dragend", dragEnd);
@@ -363,65 +354,6 @@ const screenController = () => {
            destroyer.addEventListener("dragstart", dragStart);
            destroyer.addEventListener("dragend", dragEnd);
 
-        },
-
-        userPlacesShip(event) {
-            const ship = event.target;
-            const shipname = event.target.classList[0];
-            ship.classList.add("current");
-            const playerOneShipButtons = document.querySelectorAll(".player-one-ship-buttons");
-            const playerTwoShipButtons = document.querySelectorAll(".player-two-ship-buttons");
-            
-            let limit = 0;
-            const coordinates = [];
-
-            //Limit determines how many coordinates are needed
-            if(shipname == "aircraftCarrier") {limit = 5};
-            if(shipname == "battleship") {limit = 4};
-            if(shipname == "submarine" || shipname == "cruiser") {limit = 3};
-            if(shipname == "destroyer") {limit = 2};
-
-            const handleShipPlacement = (event) => {
-                const [x, y] = event.target.classList[1];
-                coordinates.push([x, y]);
-
-                event.target.classList.add("selected");
-
-                //When all coordinates are collected disables buttons and passes to placeship:
-                if(coordinates.length == limit) {
-                    activePlayer.activePlayer.placeship(shipname, coordinates);
-
-                    if(activePlayer.name == "one") {
-                        playerOneShipButtons.forEach(button => {
-                            button.removeEventListener("click", handleShipPlacement);
-                            button.classList.remove("hover-effect");
-                        });
-                    } else if(activePlayer.name = "two") {
-                        playerTwoShipButtons.forEach(button => {
-                            button.removeEventListener("click", handleShipPlacement);
-                            button.classList.remove("hover-effect");
-                        })
-                    }
-                    ship.classList.remove("current");
-                    ship.classList.add("complete");
-                };
-
-            }
-
-            if(activePlayer.name == "one") {
-                playerOneShipButtons.forEach(button => {
-                    //Adds hover effects only after buttons are active
-                    button.classList.add("hover-effect");
-                    button.addEventListener("click", handleShipPlacement);
-                })
-            } else if(activePlayer.name == "two") {
-                console.log("works");
-                playerTwoShipButtons.forEach(button => {
-                    //Adds hover effects only after buttons are active
-                    button.classList.add("hover-effect");
-                    button.addEventListener("click", handleShipPlacement);
-                })
-            }
         },
 
         computerTurn() {
